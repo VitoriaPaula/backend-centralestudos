9aea6032ba3a0c72aea866af76baf8b6d94d9719
unknown <gfiacador@gmail.com>
2021-05-27 23:47:14 -0300

    arrumando a rota pra cadastrar curso

--- backend/API/src/routes.ts
+++ backend/API/src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
+import { CourseController } from "./controllers/CourseController";
 import { UserController } from "./controllers/UserController";
 
 
 const router = Router();
 
 const userController = new UserController();
+const courseController = new CourseController();
 
 router.post("/user",userController.create);
+router.post("/course",courseController.create);
 
 export { router }
