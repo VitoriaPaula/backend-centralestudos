47bfc2a63fa213faae062b3ed3fe8f1fb59a9c31
Vitoria Paula <vitoriapaulalinsnunes@gmail.com>
2021-06-06 16:59:07 -0300

    Ajuste front Register + add Logo

--- frontend/src/app/nav-bar/nav-bar.component.ts
+++ frontend/src/app/nav-bar/nav-bar.component.ts
@@ -11,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavBarComponent implements OnInit {
 
   esconder = true;
-  login = false;
+  login = true;
   config = false;
   cliente: Cliente;
   nome= "Vitoria";
